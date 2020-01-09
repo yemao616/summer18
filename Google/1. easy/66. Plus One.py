8bb17099be02d997d554519be360ef4aa1c028e3
Ye Mao <ymao4@ncsu.edu>
2020-01-09 12:45:29 -0500

    chek

--- Google/1. easy/66. Plus One.py
+++ Google/1. easy/66. Plus One.py
@@ -1,8 +1,8 @@
-Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+# Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
 
-You may assume the integer do not contain any leading zero, except the number 0 itself.
+# You may assume the integer do not contain any leading zero, except the number 0 itself.
 
-The digits are stored such that the most significant digit is at the head of the list.
+# The digits are stored such that the most significant digit is at the head of the list.
 
 
 
@@ -18,13 +18,19 @@ class Solution(object):
         n = len(digits)
         if digits.count(0) == n:
             return [1]
+
         for i in range(n-1, -1, -1):
             if digits[i] == 9:
                 digits[i] = 0
             else:
                 digits[i] += 1
                 break
+
         if digits[0] == 0:
             digits.insert(0, 1)
+
         return digits
         
+
+
+
